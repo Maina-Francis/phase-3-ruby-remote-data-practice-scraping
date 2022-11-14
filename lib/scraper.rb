0e11d086b9478ae4a71b77982a08c5678d429469
Francis Maina <sebastianfrancix@gmail.com>
2022-11-15 00:54:47 +0300

    completed code along

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,9 @@
-require 'nokogiri'
-require 'open-uri'
+require "nokogiri"
+require "open-uri"
 
-html = open("https://flatironschool.com/")
+html = open("https://www.scrapingbee.com/blog/web-scraping-ruby/")
+
+doc = Nokogiri::HTML(html)
+doc.css("#0-setup").text
+
+puts doc 
